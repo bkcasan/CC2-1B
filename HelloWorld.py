dedf53e0701383d5cc52571dbfb94ce281b637da
bkcasan <145944542+bkcasan@users.noreply.github.com>
2023-09-27 08:47:34 +0800

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+name = input("What is your name?") 
+age_input = input("What is your age?")
+
+age = int(age_input)
+age_next_year = age + 1
+
+print(f"Hello, {name}")
+print(f"Your age is {age_next_year} next year")
